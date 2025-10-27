863d1cf42c7ec1b726758dcde933b0617bf9a1ec
savina <savinasheoran628@gmail.com>
2025-10-27 16:19:55 +0530

    Time: 291 ms (23.70%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,11 @@
 # Write your MySQL query statement below
-Select IFNULL(
-    (Select max(salary) 
-    from Employee
-    where salary < (select max(salary) from Employee)), null) 
-    as SecondHighestSalary;
+-- SELECT 
+--     (SELECT salary 
+--      FROM (
+--          SELECT salary, 
+--                 ROW_NUMBER() OVER (ORDER BY salary DESC) AS rownum
+--          FROM (SELECT DISTINCT salary FROM Employee) AS temp
+--      ) AS temp2
+--      WHERE rownum = 2) AS SecondHighestSalary;
+
+select(select distinct salary from employee order by salary desc limit 1 offset 1) as SecondHighestSalary;
