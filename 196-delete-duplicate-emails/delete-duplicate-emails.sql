98db6614d8b03352fe1d443f86293f3a30288b00
savina <savinasheoran628@gmail.com>
2025-11-04 12:28:02 +0530

    Time: 1207 ms (5.96%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -2,3 +2,9 @@
 -- select distinct email, id from Person where id <4;
 
 delete p1 from person p1, person p2, person p3 where p1.email = p2.email and p1.id > p2.id;
+
+-- select distinct email, id from Person where email != email;
+
+-- DELETE p1
+-- FROM Person p1, Person p2, Person p3
+-- WHERE p1.email = p2.email or p2.email = p3.email AND p1.id > p2.id or p2.id > p3.id;
