dc50478c741d438219c945198651276a7cd8efb8
savina <savinasheoran628@gmail.com>
2025-11-06 13:06:35 +0530

    Time: 567 ms (89.55%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
-select d.name as Department, e.name as Employee, e.salary as Salary from Employee as e join Department as d
-on e.Departmentid = d.id
-where (Departmentid, salary) in (select Departmentid, max(salary) from Employee group by Departmentid);
+-- select d.name as Department, e.name as Employee, e.salary as Salary from Employee as e join Department as d
+-- on e.Departmentid = d.id
+-- where (Departmentid, salary) in (select Departmentid, max(salary) from Employee group by Departmentid);
+
+select d.name as Department, e.name as Employee, e.salary as Salary from Employee as e join
+Department as d on e.Departmentid = d.id where (Departmentid, salary) in (select Departmentid, max(salary) from Employee group by Departmentid);
