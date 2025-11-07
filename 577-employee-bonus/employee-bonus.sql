1768b04c0bb32d4aa0eab4fa42f2851007e2f325
savina <savinasheoran628@gmail.com>
2025-11-07 12:28:01 +0530

    Time: 859 ms (75.61%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,2 +1,5 @@
 # Write your MySQL query statement below
-select Employee.name as name, Bonus.bonus as bonus from Employee left join Bonus on Bonus.empId=Employee.empId where Bonus.bonus <1000 or Bonus.bonus is null;
+-- select Employee.name as name, Bonus.bonus as bonus from Employee left join Bonus on Bonus.empId=Employee.empId where Bonus.bonus <1000 or Bonus.bonus is null;
+
+select Employee.name as name, Bonus.bonus as bonus from Employee left join Bonus on Bonus.empid = Employee.empid
+where Bonus.bonus < 1000 or Bonus.bonus is null;
