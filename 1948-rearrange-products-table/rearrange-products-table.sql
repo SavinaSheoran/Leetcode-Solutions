75312f74c7da01c985bce9a246ca5fc2ce35a5dd
savina <savinasheoran628@gmail.com>
2025-12-02 18:04:49 +0530

    Time: 520 ms (39.59%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1948-rearrange-products-table/rearrange-products-table.sql
@@ -0,0 +1,5 @@
+select product_id,'store1' as store,store1 as price from Products where store1 is not null
+union all
+select product_id,'store2',store2 from Products where store2 is not null
+union all
+select product_id,'store3' ,store3 from Products where store3 is not null;
