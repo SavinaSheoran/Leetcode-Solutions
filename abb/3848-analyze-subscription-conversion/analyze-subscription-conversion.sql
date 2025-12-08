770353d929d9ed86ec14c00bd6836a917aecf4dc
savina <savinasheoran628@gmail.com>
2025-12-08 14:38:30 +0530

    Time: 292 ms (60.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ abb/3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
@@ -0,0 +1,15 @@
+select user_id,
+       round(avg(case when activity_type='free_trial' then activity_duration end),2)
+        as trial_avg_duration,
+       round(avg(case when activity_type='paid' then activity_duration end),2)
+        as paid_avg_duration
+from UserActivity
+where user_id in (
+                  select user_id
+                  from userActivity
+                  where activity_type in ('free_trial','paid')
+                  group by user_id
+                  having count(distinct activity_type)=2
+                  )
+group by user_id
+order  by user_id asc;
